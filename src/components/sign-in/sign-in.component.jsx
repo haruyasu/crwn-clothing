ddc4f987858721d2783a7603177539ddcc7bfbc3
haruyasu <harukun2002@gmail.com>
2019-08-21 14:33:24 +0900

    integreted firebase into app

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+
+import {signInWithGoogle} from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -44,10 +47,15 @@ class SignIn extends React.Component {
                         label='password'
                         handleChange={this.handleChange}
                         required />
-
-                    <CustomButton type='submit'>
-                        Sign In
-                    </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>
+                            Sign In
+                        </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            {' '}
+                            Sign in with Google{' '}
+                        </CustomButton>                    
+                    </div>
                 </form>
             </div>
         );
