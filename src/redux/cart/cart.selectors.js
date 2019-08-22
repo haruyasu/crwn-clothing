527b90da2d1d6f47ddf084be72e89e62446b95a1
haruyasu <harukun2002@gmail.com>
2019-08-22 18:16:53 +0900

    added checkout page

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,6 +7,11 @@ export const selectCartItems = createSelector(
     cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+    cart => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems =>
@@ -17,3 +22,12 @@ export const selectCartItemsCount = createSelector(
         )
 );
 
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+    cartItems.reduce(
+        (accumalatedQuantity, cartItem) => 
+            accumalatedQuantity + cartItem.quantity * cartItem.price,
+            0
+    )
+)
