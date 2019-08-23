68d08ad8a735fc1af42d70baae496f5a45ce6af4
haruyasu <harukun2002@gmail.com>
2019-08-23 15:53:20 +0900

    Updates

---
+++ precache-manifest.c241c05aaa82bb95cb6c49d1be73756f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe4e7d427fad1e6de317e6142935e24",
+    "url": "/haruyasu/crwn-clothing/index.html"
+  },
+  {
+    "revision": "fb570a39967559c65d0a",
+    "url": "/haruyasu/crwn-clothing/static/css/main.b42e8e35.chunk.css"
+  },
+  {
+    "revision": "79c8335e45b01723d3a3",
+    "url": "/haruyasu/crwn-clothing/static/js/2.5c2a0d97.chunk.js"
+  },
+  {
+    "revision": "fb570a39967559c65d0a",
+    "url": "/haruyasu/crwn-clothing/static/js/main.65c711f8.chunk.js"
+  },
+  {
+    "revision": "8c895445bd146a4eb4ce",
+    "url": "/haruyasu/crwn-clothing/static/js/runtime~main.70a12546.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/haruyasu/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/haruyasu/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
