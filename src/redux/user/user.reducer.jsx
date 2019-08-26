961b529ec571ede099c8367da1d54f979363fda2
haruyasu <harukun2002@gmail.com>
2019-08-26 18:12:09 +0900

    added sign up action

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -6,7 +6,7 @@ const INITIAL_STATE = {
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
-    switch(action.type) {
+    switch (action.type) {
         case UserActionTypes.SIGN_IN_SUCCESS:
             return {
                 ...state,
@@ -21,14 +21,14 @@ const userReducer = (state = INITIAL_STATE, action) => {
             };
         case UserActionTypes.SIGN_IN_FAILURE:
         case UserActionTypes.SIGN_OUT_FAILURE:
+        case UserActionTypes.SIGN_UP_FAILURE:
             return {
                 ...state,
                 error: action.payload
             };
-    
         default:
             return state;
     }
-}
+};
 
 export default userReducer;
